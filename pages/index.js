5db0932684658c013dc9f8438264ed0de7ebe646
Eric Lehmann <lehm_eric@yahoo.fr>
2022-06-02 19:00:16 +0200

    tablet layout done + started desktop layout

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import logo from "../public/logo.png";
+import photo from "../public/photo.png";
 import LoginForm from "../components/LoginForm";
 
 export default function Home() {
@@ -16,9 +17,14 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<Image src={logo} alt="Widitrade Logo" width={115} height={82} />
+				<div className={styles.logoWrapper}>
+					<Image src={logo} alt="Widitrade Logo" width={115} height={82} />
+				</div>
 				<LoginForm />
 			</main>
+			<div className={styles.photoWrapper}>
+				<Image src={photo} alt="Widitrade Photo" layout="responsive" width={803} height={1024} />
+			</div>
 		</div>
 	);
 }
