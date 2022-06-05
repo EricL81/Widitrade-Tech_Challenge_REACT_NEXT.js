b754cc76d27140a05bca1b8b3cd5cb5446e2d084
Eric Lehmann <lehm_eric@yahoo.fr>
2022-06-05 19:17:46 +0200

    rectified Sass and compiler issues

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
-import styles from "../styles/LoginForm.module.scss";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import eye from "../public/eye.png";
-import Button from "./UI/Button";
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/LoginForm.module.scss';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import eye from '../public/eye.png';
+import Button from './UI/Button';
 
 const LoginForm = () => {
 	const initialValues = {
-		user: "",
-		password: "",
+		user: '',
+		password: '',
 	};
 
 	const router = useRouter();
@@ -28,15 +28,15 @@ const LoginForm = () => {
 		const errors = {};
 
 		if (!values.user) {
-			errors.user = "User cannot be empty";
+			errors.user = 'User cannot be empty';
 		}
 
 		if (!values.password) {
-			errors.password = "Password cannot be empty";
+			errors.password = 'Password cannot be empty';
 		} else if (values.password.length < 6) {
-			errors.password = "Password must be more than 6 characters";
+			errors.password = 'Password must be more than 6 characters';
 		} else if (values.password.length > 12) {
-			errors.password = "Password cannot exceed 12 characters";
+			errors.password = 'Password cannot exceed 12 characters';
 		}
 
 		return errors;
@@ -54,32 +54,32 @@ const LoginForm = () => {
 
 	useEffect(() => {
 		if (Object.keys(errors).length === 0 && isSubmitted) {
-			router.push("/newPage");
+			router.push('/newPage');
 		}
 	}, [errors]);
 
 	return (
 		<form className={styles.loginForm} onSubmit={onSubmitHandler}>
 			<div className={styles.inputGroup}>
-				<label htmlFor="user">User</label>
-				<input type="text" id="user" name="user" onChange={onChangeHandler} placeholder="Tyson.Jakubowski@ya|" />
-				<div className={styles.error} style={{ display: errors.user ? "block" : "none" }}>
+				<label htmlFor='user'>User</label>
+				<input type='text' id='user' name='user' onChange={onChangeHandler} placeholder='Tyson.Jakubowski@ya|' />
+				<div className={styles.error} style={{ display: errors.user ? 'block' : 'none' }}>
 					{errors.user}
 				</div>
 			</div>
 			<div className={styles.inputGroup}>
-				<label htmlFor="password">Password</label>
+				<label htmlFor='password'>Password</label>
 
-				<input type={passwordIsVisible ? "text" : "password"} id="password" name="password" onChange={onChangeHandler} placeholder="Enter your password" />
+				<input type={passwordIsVisible ? 'text' : 'password'} id='password' name='password' onChange={onChangeHandler} placeholder='Enter your password' />
 				<div className={styles.imageWrapper}>
-					<Image src={eye} alt="eye" width={24} height={24} onClick={passwordHandler} />
+					<Image src={eye} alt='eye' width={24} height={24} onClick={passwordHandler} />
 				</div>
 
-				<div className={styles.error} style={{ display: errors.password ? "block" : "none" }}>
+				<div className={styles.error} style={{ display: errors.password ? 'block' : 'none' }}>
 					{errors.password}
 				</div>
 			</div>
-			<Button type="submit" classCss="login">
+			<Button type='submit' classCss='login'>
 				Login
 			</Button>
 		</form>
--- components/UI/Button.js
+++ components/UI/Button.js
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from "../../styles/UI/Button.module.scss";
+import React from 'react';
+import styles from '../../styles/UI/Button.module.scss';
 
 const Button = (props) => {
 	return (
-		<button type={props.type} onClick={props.onClick} className={`${styles.button} ${props.classCss ? `${styles[props.classCss]}` : ""}`}>
+		<button type={props.type} onClick={props.onClick} className={`${props.classCss ? `${styles[props.classCss]}` : ''}`}>
 			{props.children}
 		</button>
 	);
